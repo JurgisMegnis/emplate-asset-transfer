32fdbc2dc9d88abd407506bb58b0fc0094f8f2dd
Jurgis Megnis <jurgis@Jurgiss-MacBook-Pro.local>
2025-05-21 00:43:52 +0300

    Get the correct label and placeholder to be displayed for each step component

--- src/app/components/layout/content-panel/content-panel.component.ts
+++ src/app/components/layout/content-panel/content-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Type, OnInit } from '@angular/core';
+import { Component, Type, OnInit, Signal, input } from '@angular/core';
 import { StepsService } from '../../../service/steps.service';
 import { PrimaryButtonComponent } from '../../ui/primary-button/primary-button.component';
 import { SecondaryButtonComponent } from '../../ui/secondary-button/secondary-button.component';
@@ -6,9 +6,12 @@ import { NgComponentOutlet } from '@angular/common';
 import { TextInputComponent } from '../../ui/text-input/text-input.component';
 import { FileUploadComponent } from '../../ui/file-upload/file-upload.component';
 import { FileFormService } from '../../../service/file-form.service';
+import { Step } from '../../../data/step.model';
+
 
 // Union type with all of the possible components
 type DynamicComponent = TextInputComponent | FileUploadComponent;
+type ComponentDataType = {type: Type<DynamicComponent>, inputs: Record<string, string>};
 
 @Component({
   selector: 'app-content-panel',
@@ -21,8 +24,13 @@ type DynamicComponent = TextInputComponent | FileUploadComponent;
   styleUrl: './content-panel.component.scss'
 })
 export class ContentPanelComponent implements OnInit {
+  // Get the active step
+  private activeStep: Step | undefined;
+
   // Components array that contains input component classes that are necessary for the currently active step
   protected components: Type<DynamicComponent>[] | null = null;
+
+  protected componentData: ComponentDataType[] | null = null;
 
   // Registry that maps component strings (from the data source) to their corresponding component classes
   private componentMap: Record<string, Type<DynamicComponent>> = {
@@ -33,17 +41,70 @@ export class ContentPanelComponent implements OnInit {
   constructor(public stepsService: StepsService, private fileFormService: FileFormService ) {}
 
   ngOnInit(): void {
+    // Get the currently active step
+    this.activeStep = this.stepsService.activeStepSignal();
+    // Early return if there is no active step
+    if (!this.activeStep) {return}
+
     // Pass the input component classes that are necessary for the currently active step to the components array
-    this.components = this.stepsService.activeStepSignal()?.dataInputComponents
+    this.components = this.activeStep.components
       .map(item => this.componentMap[item as keyof typeof this.componentMap])
       .filter((component): component is Type<DynamicComponent> => component !== undefined) || null;
+
+    this.componentData = this.components?.map((item, index) => {
+      let inputConfig: Record<string, string>;
+
+      if (item === TextInputComponent) {
+        const labelValue = this.activeStep?.componentInputs?.['label']?.[index] || '';
+        const placeholderValue = this.activeStep?.componentInputs?.['placeholder']?.[index] || '';
+
+        inputConfig = {
+          label: labelValue,
+          placeholder: placeholderValue
+        };
+      } else {
+        inputConfig = {};
+      }
+
+      return {
+        type: item,
+        inputs: inputConfig
+      }
+    }) || null
   }
 
   // Pass the input component classes that are necessary for the currently active step to the components array
   protected dynamicComponentAssign() {
-    this.components = this.stepsService.activeStepSignal()?.dataInputComponents
+    // Get the currently active step
+    this.activeStep = this.stepsService.activeStepSignal();
+    // Early return if there is no active step
+    if (!this.activeStep) {return}
+
+    // Pass the input component classes that are necessary for the currently active step to the components array
+    this.components = this.activeStep.components
       .map(item => this.componentMap[item as keyof typeof this.componentMap])
       .filter((component): component is Type<DynamicComponent> => component !== undefined) || null;
+
+    this.componentData = this.components?.map((item, index) => {
+      let inputConfig: Record<string, string>;
+
+      if (item === TextInputComponent) {
+        const labelValue = this.activeStep?.componentInputs?.['label']?.[index] || '';
+        const placeholderValue = this.activeStep?.componentInputs?.['placeholder']?.[index] || '';
+
+        inputConfig = {
+          label: labelValue,
+          placeholder: placeholderValue
+        };
+      } else {
+        inputConfig = {};
+      }
+
+      return {
+        type: item,
+        inputs: inputConfig
+      }
+    }) || null
   }
 
   submitForm() {
--- src/app/data/steps-data.ts
+++ src/app/data/steps-data.ts
@@ -1,15 +1,15 @@
 import { Step } from "./step.model";
 
 export const STEPS: Step[]= [
-    new Step('00', 'Welcome', 'Let\'s get started', [], true, 'icons/step-idle/00.svg', 'icons/step-active/00.svg', true, false, ['TextInputComponent']),
-    new Step('01', 'Logo', 'Vector files', [], true, 'icons/step-idle/01.svg', 'icons/step-active/01.svg', false, false, ['FileUploadComponent']),
-    new Step('02', 'Login screen', 'Image in jpg', [], true, 'icons/step-idle/02.svg', 'icons/step-active/02.svg', false, false, ['FileUploadComponent']),
-    new Step('03', 'Color', 'Hex codes', [], true, 'icons/step-idle/03.svg', 'icons/step-active/03.svg', false, false, ['TextInputComponent', 'TextInputComponent']),
-    new Step('04', 'App icon', 'Vector files', [], false, 'icons/step-idle/04.svg', 'icons/step-active/04.svg', false, false, ['FileUploadComponent']),
-    new Step('05', 'Home tab header', 'Image in jpg', [], false, 'icons/step-idle/05.svg', 'icons/step-active/05.svg', false, false, ['FileUploadComponent']),
-    new Step('06', 'Tab header', 'Image in jpg', [], false, 'icons/step-idle/06.svg', 'icons/step-active/06.svg', false, false, ['FileUploadComponent']),
-    new Step('07', 'Onboarding slides', 'Images in jpg', [], false, 'icons/step-idle/07.svg', 'icons/step-active/07.svg', false, false, ['FileUploadComponent']),
-    new Step('08', 'Loyalty button icon', 'Vector files', [], false, 'icons/step-idle/08.svg', 'icons/step-active/08.svg', false, false, ['FileUploadComponent']),
-    new Step('09', 'Other files', 'Any format', [], false, 'icons/step-idle/09.svg', 'icons/step-active/09.svg', false, false, ['FileUploadComponent']),
-    new Step('10', 'Check & submit', 'Celebrate', [], false, 'icons/step-idle/10.svg', 'icons/step-active/10.svg', false, false, []),
+    new Step('00', 'Welcome', 'Let\'s get started', [], true, 'icons/step-idle/00.svg', 'icons/step-active/00.svg', true, false, ['TextInputComponent'], {label: ['yeye'], placeholder: ['yeye']}),
+    new Step('01', 'Logo', 'Vector files', [], true, 'icons/step-idle/01.svg', 'icons/step-active/01.svg', false, false, ['FileUploadComponent'], {}),
+    new Step('02', 'Login screen', 'Image in jpg', [], true, 'icons/step-idle/02.svg', 'icons/step-active/02.svg', false, false, ['FileUploadComponent'], {}),
+    new Step('03', 'Color', 'Hex codes', [], true, 'icons/step-idle/03.svg', 'icons/step-active/03.svg', false, false, ['TextInputComponent', 'TextInputComponent'], {label: ['yeye', 'yeyeyeyeye'], placeholder: ['yeye', 'yeyeyeyeye']}),
+    new Step('04', 'App icon', 'Vector files', [], false, 'icons/step-idle/04.svg', 'icons/step-active/04.svg', false, false, ['FileUploadComponent'], {}),
+    new Step('05', 'Home tab header', 'Image in jpg', [], false, 'icons/step-idle/05.svg', 'icons/step-active/05.svg', false, false, ['FileUploadComponent'], {}),
+    new Step('06', 'Tab header', 'Image in jpg', [], false, 'icons/step-idle/06.svg', 'icons/step-active/06.svg', false, false, ['FileUploadComponent'], {}),
+    new Step('07', 'Onboarding slides', 'Images in jpg', [], false, 'icons/step-idle/07.svg', 'icons/step-active/07.svg', false, false, ['FileUploadComponent'], {}),
+    new Step('08', 'Loyalty button icon', 'Vector files', [], false, 'icons/step-idle/08.svg', 'icons/step-active/08.svg', false, false, ['FileUploadComponent'], {}),
+    new Step('09', 'Other files', 'Any format', [], false, 'icons/step-idle/09.svg', 'icons/step-active/09.svg', false, false, ['FileUploadComponent'], {}),
+    new Step('10', 'Check & submit', 'Celebrate', [], false, 'icons/step-idle/10.svg', 'icons/step-active/10.svg', false, false, [], {}),
 ]
