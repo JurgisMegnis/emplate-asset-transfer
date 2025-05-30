d11b111240fa1a104d82676aee3e7a82a109c1a8
Jurgis Megnis <jurgis@Jurgiss-MacBook-Pro.local>
2025-05-30 10:24:34 +0300

    Fix as Github Actions failed to deploy

--- src/app/service/asset-transfer.service.spec.ts
+++ src/app/service/asset-transfer.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { FileFormService } from './asset-transfer.service';
+import { AssetTransferService } from './asset-transfer.service';
 
 describe('FileFormService', () => {
-  let service: FileFormService;
+  let service: AssetTransferService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(FileFormService);
+    service = TestBed.inject(AssetTransferService);
   });
 
   it('should be created', () => {
